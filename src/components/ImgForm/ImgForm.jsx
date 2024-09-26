dde310fad9efd85eddb987a67df32213fb87a30b
hatimitu0064 <ansenseofpurpose@gmail.com>
2024-09-26 15:50:03 +0900

    画像投稿用のフォームを作成

---
+++ src/components/ImgForm/ImgForm.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import './ImgForm.scss'
+
+const ImgForm = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
+
+  const handleImgChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImg(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className="img-form-container">
+      {setSelectedImg && (
+        <div className="img-container">
+          {/* <p>選択した画像：</p> */}
+          <img src={selectedImg} className="selectImg" />
+        </div>
+      )}
+      <form className="input-container">
+        <input id="fileInput" type="file" accept="image/*" onChange={handleImgChange} />
+				<label htmlFor="fileInput" className="fileInput-label">画像を選択</label>
+      </form>
+    </div>
+  );
+};
+
+export default ImgForm;
